c43c065aef143d5d9df8680c7f3a690419a90d71
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-12-22 15:12:29 -0700

    uhuh

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Createpak",
-      customCss: ["@fontsource/inter/400.css", "@fontsource/inter/600.css"],
+      customCss: [
+        "./src/styles/custom.css",
+        "@fontsource/inter/400.css",
+        "@fontsource/inter/600.css",
+      ],
       social: { github: "https://github.com/ld3z/site6" },
       logo: { src: "./src/assets/createpak_logo.png", replacesTitle: true },
       components: {
