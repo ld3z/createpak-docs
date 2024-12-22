105da39d196b6ea8b17f2d3f4620842cd9b94ddf
ld3zian <42660576+ld3z@users.noreply.github.com>
2024-12-22 14:49:11 -0700

    add logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-  site: 'https://ld3z.github.io',
-  base: '/site6',
+  site: "https://ld3z.github.io",
+  base: "/site6",
   integrations: [
     starlight({
-      title: 'Createpak',
-      social: { github: 'https://github.com/ld3z/site6' },
+      title: "Createpak",
+      social: { github: "https://github.com/ld3z/site6" },
+      logo: { src: "./src/assets/createpak_logo.png" },
       components: {
-        Head: './src/components/Head.astro',
+        Head: "./src/components/Head.astro",
       },
     }),
   ],
