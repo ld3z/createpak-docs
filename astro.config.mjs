02034e736f6ff000a80f3f314f6de74e98b23fff
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-01-03 18:59:39 -0700

    add curseforge icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,24 @@ import starlight from "@astrojs/starlight";
 
 import vercel from "@astrojs/vercel";
 
+import icon from "astro-icon";
+
 export default defineConfig({
   site: "https://createpak-docs.vercel.app",
 
-  integrations: [
-    starlight({
-      title: "Createpak",
-      customCss: [
-        "./src/styles/custom.css",
-        "@fontsource-variable/inter",
-      ],
-      social: { github: "https://github.com/ld3z/createpak-docs" },
-      logo: { src: "./src/assets/createpak_logo.webp", replacesTitle: true },
-      favicon: '/favicon.png',
-      components: {
-        Head: "./src/components/Head.astro",
-      },
-    }),
-  ],
+  integrations: [starlight({
+    title: "Createpak",
+    customCss: [
+      "./src/styles/custom.css",
+      "@fontsource-variable/inter",
+    ],
+    social: { github: "https://github.com/ld3z/createpak-docs" },
+    logo: { src: "./src/assets/createpak_logo.webp", replacesTitle: true },
+    favicon: '/favicon.png',
+    components: {
+      Head: "./src/components/Head.astro",
+    },
+  }), icon()],
 
   adapter: vercel(),
 });
