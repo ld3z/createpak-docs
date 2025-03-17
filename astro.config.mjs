49c3a0379e4c6ae92fedab3f136955e9fd42a654
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-03-16 22:14:22 -0700

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,24 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://createpak-docs.vercel.app",
 
-  integrations: [starlight({
-    title: "Createpak",
-    customCss: [
-      "./src/styles/custom.css",
-      "@fontsource-variable/inter",
-      "./src/styles/inline.css",
-      "./src/styles/table.css",
-    ],
-    social: { github: "https://github.com/ld3z/createpak-docs" },
-    logo: { src: "./src/assets/createpak_logo.webp", replacesTitle: true },
-    favicon: '/favicon.png',
-    components: {
-      Head: "./src/components/Head.astro",
-    },
-  }), icon()],
+  integrations: [
+    starlight({
+      title: "Createpak",
+      customCss: [
+        "./src/styles/custom.css",
+        "@fontsource-variable/inter",
+        "./src/styles/inline.css",
+        "./src/styles/table.css",
+      ],
+      social: { github: "https://github.com/ld3z/createpak-docs" },
+      logo: { src: "./src/assets/createpak_logo.webp", replacesTitle: true },
+      favicon: "/favicon.png",
+      components: {
+        Head: "./src/components/Head.astro",
+      },
+    }),
+    icon(),
+  ],
 
   adapter: vercel(),
 });
