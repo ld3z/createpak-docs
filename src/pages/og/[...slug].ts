86cf2a1a6f15f1588f27ec1c6711a7f1c48efe57
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-03-16 21:48:00 -0700

    update

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,29 +1,35 @@
-import { getCollection } from 'astro:content'
-import { OGImageRoute } from 'astro-og-canvas'
+import { getCollection } from 'astro:content';
+import { OGImageRoute } from 'astro-og-canvas';
 
-// Get all entries from the `docs` content collection.
-const entries = await getCollection('docs')
-
-// Map the entry array to an object with the page ID as key and the
-// frontmatter data as value.
-const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]))
+const entries = await getCollection('docs');
+const pages = Object.fromEntries(
+  entries.map(({ data, id }) => [id, { data }])
+);
 
 export const { getStaticPaths, GET } = OGImageRoute({
-  // Pass down the documentation pages.
   pages,
-  // Define the name of the parameter used in the endpoint path, here `slug`
-  // as the file is named `[...slug].ts`.
   param: 'slug',
-  // Define a function called for each page to customize the generated image.
-  getImageOptions: (_path, page: (typeof pages)[number]) => {
-    return {
-      // Use the page title and description as the image title and description.
-      title: page.data.title,
-      description: page.data.description,
-      // Customize various colors and add a border.
-      bgGradient: [[24, 24, 27]],
-      border: { color: [63, 63, 70], width: 20 },
-      padding: 120,
-    }
-  },
-})
+  getImageOptions: (_path, page: any) => ({
+    title: page.data.title,
+    description: page.data.description,
+    logo: {
+      path: './src/assets/createpak_logo.webp',
+      size: [100, 100],
+    },
+    border: { color: [63, 63, 70], width: 2 },
+    bgGradient: [[24, 24, 27]],
+    font: {
+      title: {
+        size: 70,
+        families: ['Inter'],
+        weight: 'Medium',
+      },
+      description: {
+        size: 40,
+        families: ['Inter'],
+        weight: 'Normal',
+      },
+    },
+    padding: 60,
+  }),
+});
